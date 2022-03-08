2506db818867c7ee4537c787ddce8645a58f3297
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2022-03-08 16:46:45 -0300

    08 | Makes example of Dynamic Navigation

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -13,6 +13,7 @@ export default function Inicio() {
             <Navegador destino={'/exemplo'} texto={'Exemplo'} cor={'#9400d3'}/>
             <Navegador destino={'/jsx'} texto={'JSX'} cor={'crimson'} />
             <Navegador destino={'/navegacao/'} texto={'Navegação #01'} cor={'green'} />
+            <Navegador destino={'/cliente/sp-2/123'} texto={'Navegação #02'} cor={'blue'} />
         </div>
     );
 }
